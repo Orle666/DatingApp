9ec54ce609ea816942bc35a0f107eb77665314bc
odurdevic <orle.durdevic@infobip.com>
2022-03-15 19:40:08 +0100

    Angulat changes CORS

--- API/client/src/app/app.component.ts
+++ API/client/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'The DatingApp';
+  users : any; // ovo znaci da moze biti bilo koji tip podatka.
+
+constructor(private http: HttpClient) {}
+  ngOnInit() {
+    //throw new Error('Method not implemented.');
+    this.getUsers;
+  } 	    
+
+  getUsers() {
+
+    this.http.get('https://localhost:5001/api/users').subscribe(response =>{
+      this.users=response;
+    },
+    error =>{
+      console.log(error);
+    }
+    ) //ovo je OBSERVABLE, tj. mora se neko subscribati da gleda na njih, inace ne rade nista
+
+  }
+
 }
